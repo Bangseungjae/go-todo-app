c4d324497564080db5186482ddf82dcb700d3280
Bangseungjae <tmdwo5825@naver.com>
2024-09-30 20:57:12 +0900

    Study 24.09.30

---
+++ handler/add_task.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"Bangseungjae/go-todo-app/entity"
+	"Bangseungjae/go-todo-app/store"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type AddTask struct {
+	Store     *store.TaskStore
+	Validator *validator.Validate
+}
+
+func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var b struct {
+		Title string `json:"title" validate:"required"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+	if err := at.Validator.Struct(b); err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	t := &entity.Task{
+		Title:   b.Title,
+		Status:  "todo",
+		Created: time.Now(),
+	}
+	id, err := store.Tasks.Add(t)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	rsp := struct {
+		ID int `json:"id"`
+	}{ID: int(id)}
+	RespondJSON(ctx, w, rsp, http.StatusOK)
+}
