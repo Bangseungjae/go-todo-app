a9f7529ca9c3526e8e0d47e41d5317e94675a8a5
Bangseungjae <tmdwo5825@naver.com>
2024-10-07 02:42:42 +0900

    jwt

---
+++ auth/jwt.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"Bangseungjae/go-todo-app/clock"
+	"Bangseungjae/go-todo-app/entity"
+	"context"
+	_ "embed"
+	"fmt"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+)
+
+//go:embed cert/secret.pem
+var rawPrivKey []byte
+
+//go:embed cert/public.pem
+var rawPubKey []byte
+
+type JWTer struct {
+	PrivateKey, PublicKey jwk.Key
+	Store                 Store
+	Clocker               clock.Clocker
+}
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . Store
+type Store interface {
+	Save(ctx context.Context, key string, userID entity.UserID) error
+	Load(ctx context.Context, key string) (entity.UserID, error)
+}
+
+func NewJWTer(s Store, c clock.Clocker) (*JWTer, error) {
+	j := &JWTer{Store: s, Clocker: c}
+	privKey, err := parse(rawPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: private key: %w", err)
+	}
+	pubKey, err := parse(rawPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: public key: %w", err)
+	}
+	j.PrivateKey = privKey
+	j.PublicKey = pubKey
+	return j, nil
+}
+
+func parse(rawKey []byte) (jwk.Key, error) {
+	key, err := jwk.ParseKey(rawKey, jwk.WithPEM(true))
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
